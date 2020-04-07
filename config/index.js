c910ac7576fa57962b31aebf3f8f0c0b10dc36ff
GuHai <Private2.1>
2020-04-07 08:33:02 +0800

    跨域配置

--- config/index.js
+++ config/index.js
@@ -10,7 +10,15 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': { // 请求的代称，写在Axios里的BaseUrl
+        target: 'http://localhost:8080',  // 真实请求URl
+        changeOrigin: true, // 允许跨域
+        pathRewrite: {  // 替换，通配/api的替换成/
+          '^/api': '/'
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
